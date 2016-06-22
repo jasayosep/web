56bee31bf745438a7b661f6de8c6ba7e8d0feeb9
jasayosep <jasayosep@gmail.com>
2016-06-22 23:54:56 +0700

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,51 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Linux (i686)
+--
+-- Host: localhost    Database: web_ku
+-- ------------------------------------------------------
+-- Server version	5.7.12-0ubuntu1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `berita`
+--
+
+DROP TABLE IF EXISTS `berita`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `berita` (
+  `id_berita` int(5) DEFAULT NULL,
+  `judul_berita` varchar(100) NOT NULL DEFAULT '',
+  `isi_berita` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `berita`
+--
+
+LOCK TABLES `berita` WRITE;
+/*!40000 ALTER TABLE `berita` DISABLE KEYS */;
+INSERT INTO `berita` VALUES (1,'Berita Pertama','Ini isi berita pertamaku :D'),(2,'Berita Kedua','Ini isi berita keduaku :P'),(3,'Berita Ketiga','Ini isi berita ketigaku :-)');
+/*!40000 ALTER TABLE `berita` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-06-22 23:54:39
